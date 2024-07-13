f428f554d7c9fd5fbbe09c9f6a7f8538c6e1c8ee
DPdotexe <danielepuleri97@gmail.com>
2024-07-13 18:32:30 +0200

    some changes

--- src/pages/CouponList/Coupon.jsx
+++ src/pages/CouponList/Coupon.jsx
@@ -1,53 +1,54 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FaArrowLeft } from 'react-icons/fa';
+import { FaArrowLeft, FaCog, FaHome, FaEnvelope, FaTicketAlt } from 'react-icons/fa';
 import './Coupon.css';
 
 function Coupon() {
-  // array coupon 
   const coupons = [
-    { id: 1, name: 'Coupon 1', description: 'Descrizione del Coupon 1' },
-    { id: 2, name: 'Coupon 2', description: 'Descrizione del Coupon 2' },
-    { id: 3, name: 'Coupon 3', description: 'Descrizione del Coupon 3' },
-    { id: 4, name: 'Coupon 4', description: 'Descrizione del Coupon 4' },
-    { id: 5, name: 'Coupon 5', description: 'Descrizione del Coupon 5' },
-    { id: 6, name: 'Coupon 6', description: 'Descrizione del Coupon 6' },
-    { id: 7, name: 'Coupon 7', description: 'Descrizione del Coupon 7' },
-    { id: 8, name: 'Coupon 8', description: 'Descrizione del Coupon 8' },
+    { id: 1, name: 'Coupon 1', description: 'Descrizione del Coupon 1', discount: '-20€', image: '/images/coupon1.jpg' },
+    { id: 2, name: 'Coupon 2', description: 'Descrizione del Coupon 2', discount: '-15€', image: '/images/coupon2.jpg' },
+    { id: 3, name: 'Coupon 3', description: 'Descrizione del Coupon 3', discount: '-10€', image: '/images/coupon3.jpg' },
+    { id: 4, name: 'Coupon 4', description: 'Descrizione del Coupon 4', discount: '-30€', image: '/images/coupon4.jpg' },
+    { id: 5, name: 'Coupon 5', description: 'Descrizione del Coupon 5', discount: '-25€', image: '/images/coupon5.jpg' },
+    { id: 6, name: 'Coupon 6', description: 'Descrizione del Coupon 6', discount: '-35€', image: '/images/coupon6.jpg' },
+    { id: 7, name: 'Coupon 7', description: 'Descrizione del Coupon 7', discount: '-18€', image: '/images/coupon7.jpg' },
+    { id: 8, name: 'Coupon 8', description: 'Descrizione del Coupon 8', discount: '-12€', image: '/images/coupon8.jpg' },
   ];
 
-  // funzione click menu
   const handleMenuClick = (menuItem) => {
     alert(`Hai cliccato su ${menuItem}`);
   };
 
   return (
-    <div className="coupon-list"> 
+    <div className="coupon-list">
       <Link to="/" className="back-link">
-        <FaArrowLeft className="icon" /> 
+        <FaArrowLeft className="icon" />
       </Link>
-      <h2>Lista Coupon</h2> 
+      <h2>Lista Coupon</h2>
       <ul>
-
         {coupons.map((coupon) => (
-          <li key={coupon.id} className="coupon-item"> 
-            <h3>{coupon.name}</h3> 
-            <p>{coupon.description}</p> 
+          <li key={coupon.id} className="coupon-item">
+            <img src={coupon.image} alt={`Immagine del coupon ${coupon.name}`} className="coupon-image" />
+            <div className="coupon-details">
+              <h3>{coupon.name}</h3>
+              <p>{coupon.description}</p>
+            </div>
+            <p className="coupon-discount">{coupon.discount}</p>
           </li>
         ))}
       </ul>
-      <div className="menu"> 
+      <div className="menu">
         <button onClick={() => handleMenuClick('Menu 1')}>
-          <i className="icon">i</i> Menu 1
+          <FaHome className="icon" />
         </button>
         <button onClick={() => handleMenuClick('Menu 2')}>
-          <i className="icon">i</i> Menu 2
-        </button>
+          <FaTicketAlt className="icon" />
+        </button>      
         <button onClick={() => handleMenuClick('Menu 3')}>
-          <i className="icon">i</i> Menu 3
+          <FaEnvelope className="icon" />
         </button>
         <button onClick={() => handleMenuClick('Menu 4')}>
-          <i className="icon">i</i> Menu 4
+          <FaCog className="icon" />
         </button>
       </div>
     </div>
